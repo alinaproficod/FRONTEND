9f3b0e111171780ee1e348c5fd33385b3731eb7f
Alina <131201532+alinaproficod@users.noreply.github.com>
2023-07-11 23:40:37 +0200

    Add files via upload

---
+++ HOMEWORKjs/snuliom.js
@@ -0,0 +1,11 @@
+let number = prompt("Введите число:");
+if (number > 0){
+    console.log("Число больше нуля.");
+}else if(number < 0){
+    console.log("Число меньше нуля.")
+
+    
+}else {
+
+    console.log("Число равно нулю.")
+}
